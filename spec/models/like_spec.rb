2f4359892492c6face6dccf0558b650e93211d74
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-12-12 22:11:56 +0200

    Add like test for integration specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
-<<<<<<< HEAD
-RSpec.describe Like, type: :model do
-  @user = User.create(
-    name: 'Doe', photo: 'https://johndoe.com/me.png',
-    bio: 'I am John Doe.', posts_counter: 0
-  )
-
-  post = Post.create(
-    author: @user, title: 'About', text: 'About me', comments_counter: 0,
-    likes_counter: 0
-  )
-
-  Like.create(post:, author: @user)
-
-  context 'update_likes_counter' do
-    it ' increment likes_counter' do
-      expect(Post.find(post.id).likes_counter).eql?(post.likes_counter + 1)
-    end
-=======
 describe Like, type: :model do
   before :each do
     @author = User.new(name: 'Edi', photo: 'https://avatars.githubusercontent.com/u/105216647?v=4',
@@ -34,6 +15,5 @@ describe Like, type: :model do
 
   it 'will have the Posts likes_counter through update_comments_counter ' do
     expect(@post.likes_counter).to eq 1
->>>>>>> a84e878566ac4c3820d02ce9889d3f621b67f9fa
   end
 end
