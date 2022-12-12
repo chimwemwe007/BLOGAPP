8c091234bfcca9a4930ad08a1c7b0f60822b6c98
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-12-12 22:16:57 +0200

    Add user index show post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,64 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  let(:author) { User.create(name: 'test', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'test', posts_counter: 2) }
-
+RSpec.describe User, type: :model do
   subject do
-    Post.new(title: 'test', author_id: author.id, comments_counter: 2, likes_counter: 2)
+    User.new(name: 'test', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'test', posts_counter: 2)
   end
 
-  it 'is valid with valid attributes' do
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'post counter should be greater than 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'post counter should be greater than 0' do
+    subject.posts_counter = 1
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a title' do
-    subject.title = nil
+  it 'post counter should be an integer' do
+    subject.posts_counter = 1.2
     expect(subject).to_not be_valid
   end
-
-  it 'is not valid without a author_id' do
-    subject.author_id = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a comments_counter' do
-    subject.comments_counter = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a likes_counter' do
-    subject.likes_counter = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with a title longer than 250 characters' do
-    subject.title = 'a' * 251
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with a comments_counter not being an integer' do
-    subject.comments_counter = 'a'
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with a comments_counter being less than 0' do
-    subject.comments_counter = -1
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with a likes_counter not being an integer' do
-    subject.likes_counter = 'a'
-    expect(subject).to_not be_valid
-  end
-
   # add tests for your custom methods here
-  it 'should return the most recent comments' do
-    post = Post.create(title: 'test', author_id: author.id, comments_counter: 2, likes_counter: 2)
-    comment2 = Comment.create(text: 'test', author_id: author.id, post_id: post.id)
-    comment3 = Comment.create(text: 'test', author_id: author.id, post_id: post.id)
-    comment4 = Comment.create(text: 'test', author_id: author.id, post_id: post.id)
-    comment5 = Comment.create(text: 'test', author_id: author.id, post_id: post.id)
-    comment6 = Comment.create(text: 'test', author_id: author.id, post_id: post.id)
-    expect(post.most_recent_comments).to eq([comment6, comment5, comment4, comment3, comment2])
+  it 'should return the most recent posts' do
+    user = User.create(name: 'test', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'test',
+                       posts_counter: 2)
+    post2 = Post.create(title: 'test', author_id: user.id, comments_counter: 2, likes_counter: 2)
+    post3 = Post.create(title: 'test', author_id: user.id, comments_counter: 2, likes_counter: 2)
+    post4 = Post.create(title: 'test', author_id: user.id, comments_counter: 2, likes_counter: 2)
+    expect(user.most_recent_posts).to eq([post4, post3, post2])
   end
 end
