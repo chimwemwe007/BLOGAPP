901e0aa2bcefd47eb5522cd2b585b8dc5b895dac
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-12-15 14:54:00 +0200

    .

--- db/migrate/20221215065050_create_post.rb
+++ db/migrate/20221215065050_create_post.rb
@@ -2,10 +2,9 @@ class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :text
-      t.integer :comments_counter
+      t.string :text
       t.integer :likes_counter
-
+      t.integer :comments_counter
       t.timestamps
     end
   end
--- db/migrate/20221215065241_create_comment.rb
+++ db/migrate/20221215065241_create_comment.rb
@@ -1,8 +1,7 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :text
-
+      t.string :text
       t.timestamps
     end
   end
