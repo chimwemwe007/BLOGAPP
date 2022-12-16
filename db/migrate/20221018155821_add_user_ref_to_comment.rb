c87b7dbbbb5231a90639aa6502aa69793e490fde
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-12-16 19:44:30 +0200

    Resolve linter issue

--- db/migrate/20221018155821_add_user_ref_to_comment.rb
+++ db/migrate/20221018155821_add_user_ref_to_comment.rb
@@ -1,4 +1,4 @@
-class AddUserRefToComment < ActiveRecord::Migration[7.0]
+ class AddUserRefToComment < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :user, null: false, foreign_key: true
   end
